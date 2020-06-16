1b97ea2522d56e542f8420c1e0bcf1cbca893d31
박혜미 <haymee@naturemobility.com>
2020-06-16 23:44:53 +0900

    Update 2020-06-16T14:44:51.924Z

--- js/timer.js
+++ js/timer.js
@@ -3,7 +3,9 @@ const timer = timerContainer.querySelector('h3');
 
 function setTime() {
   const now = new Date();
-  timer.innerText = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+  timer.innerText = `${now.getHours() < 10 ? `0${now.getHours()}` : now.getHours()}:${now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes()}:${
+    now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds()
+  }`;
 }
 
 function init() {
